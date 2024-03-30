22ade94c2cf2aff70109a19a3f172d8801a7257d
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-03-30 14:33:31 +0200

    range example

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,7 +10,7 @@ main :: IO ()
 main = do
   rootDir <- getCurrentDirectory
   let basePath = rootDir ++ "/test/data"
-      fileName = "list_alloc"
+      fileName = "list_add"
   tokens <- scanTokens <$> readFile (basePath ++ "/" ++ fileName ++ ".adpl")
   -- print tokens
   let progAst = parseProg tokens
