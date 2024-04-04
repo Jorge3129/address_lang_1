26412a861c32d541c818f3ebb91441920fdefa81
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-04-04 15:34:16 +0300

    fix free vars

--- src/Vm/MemUtils.hs
+++ src/Vm/MemUtils.hs
@@ -64,12 +64,14 @@ constructList vals vm =
     consList' _ [] vm_ = vm_
 
 freeVars :: VM -> VM
-freeVars vm = vm
-
--- freeVars vm =
---   let curScope = length (vmCalls vm)
---       locals = [addr | (varName, addr) <- Map.toList (varsMap vm), getVarScope varName == curScope]
---    in vm {memory = [if addr `elem` locals then NilVal else val | (addr, val) <- zip [0 ..] (memory vm)]}
+freeVars vm =
+  let curScope = length (vmCalls vm)
+      locals = [(varName, addr) | (varName, addr) <- Map.toList (varsMap vm), getVarScope varName == curScope]
+      localAddrs = map snd locals
+   in vm
+        { memory = [if addr `elem` localAddrs then NilVal else val | (addr, val) <- zip [0 ..] (memory vm)],
+          varsMap = foldl' (flip Map.delete) (varsMap vm) (map fst locals)
+        }
 
 getRefsToAddr :: Int -> VM -> [Int]
 getRefsToAddr addr vm = [i | (c, i) <- zip (memory vm) [0 :: Int ..], isPointer c && asInt c == addr]
