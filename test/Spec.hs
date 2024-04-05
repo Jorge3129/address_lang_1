77ea06a0077f5d486b704a19f380ef99923134b5
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-04-05 21:01:55 +0300

    use list literals in bin tree example

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,7 +10,7 @@ main :: IO ()
 main = do
   rootDir <- getCurrentDirectory
   let basePath = rootDir ++ "/test/data"
-      fileName = "list_literal"
+      fileName = "bin_tree"
   tokens <- scanTokens <$> readFile (basePath ++ "/" ++ fileName ++ ".adpl")
   -- print tokens
   let progAst = parseProg tokens
