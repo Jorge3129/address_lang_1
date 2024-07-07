cb2b673ddae6900f5e80193833ae353d2babf660
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-07-07 22:47:02 +0300

    add list_map cpp version

--- test/data/list_map/list_map.cpp
+++ test/data/list_map/list_map.cpp
@@ -6,20 +6,51 @@ struct node {
     node* next;
 };
 
-// this is a builtin function in ADPL
-void printList(node** head) {
-    std::cout << '[';
-    for (auto cur = *head; cur != nullptr; cur = cur->next) {
-        std::cout << cur->value;
-        if (cur->next != nullptr) std::cout << ',';
-    }
-    std::cout << ']' <<  std::endl;
+// TODO rename fn
+int double_(int val) {
+    return val * 2;
 }
 
 node** list_empty() {
     auto s = new node*;
     *s = nullptr;
     return s;
+}
+
+void list_add(int val, node** head) {
+    auto last_node = *head;
+
+    if (*head != nullptr) {
+        for (auto i = *head; i != nullptr; i = i->next) {
+            last_node = i;
+        }
+    }
+    auto new_node = new node;
+    if (*head == nullptr) {
+        *head = new_node;
+    } else {
+        last_node->next = new_node;
+    }
+    new_node->next = nullptr;
+    new_node->value = val;
+}
+
+node** map(int (*f)(int val), node** head) {
+    auto r = list_empty();
+
+    if (*head == nullptr) {
+        return r;
+    }
+
+// TODO unify pi
+    for (auto i = *head; i != nullptr; i = i->next) {
+        auto val = i->value;
+        auto new_val = (*f)(val);
+
+        list_add(new_val, r);
+    }
+
+    return r;
 }
 
 node** create_list(std::vector<int> values) {
@@ -42,12 +73,52 @@ node** create_list(std::vector<int> values) {
     return list;
 }
 
+// this is a builtin function in ADPL
+void printList(node** head) {
+    std::cout << '[';
+    for (auto cur = *head; cur != nullptr; cur = cur->next) {
+        std::cout << cur->value;
+        if (cur->next != nullptr) std::cout << ',';
+    }
+    std::cout << ']' <<  std::endl;
+}
+
 int main() {
-    node** list = create_list({1,2,3,4,5});
-    printList(list);
-    printList(create_list({}));
-    printList(create_list({1}));
-    printList(create_list({1,2}));
+    {
+        node** list = create_list({1,2,3,4,5});
+        printList(list);
+
+        auto new_list = map(&double_,list);
+
+        printList(new_list);
+
+        delete list;
+        delete new_list;
+    }
+
+    {
+        node** list = create_list({});
+        printList(list);
+
+        auto new_list = map(&double_,list);
+
+        printList(new_list);
+
+        delete list;
+        delete new_list;
+    }
+
+        {
+        node** list = create_list({1});
+        printList(list);
+
+        auto new_list = map(&double_,list);
+
+        printList(new_list);
+
+        delete list;
+        delete new_list;
+    }
 
     return 0;
 }
