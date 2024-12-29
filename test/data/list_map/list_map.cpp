fc5b2718dfd36a82f61e5789c1a27d42ccf485fd
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-12-29 15:18:21 +0200

    update list_map examples

--- test/data/list_map/list_map.cpp
+++ test/data/list_map/list_map.cpp
@@ -2,93 +2,93 @@
 #include <vector>
 
 struct node {
-    int value;
-    node* next;
+  int value;
+  node* next;
 };
 
-node** list_empty() {
-    auto s = new node*;
-    *s = nullptr;
-    return s;
+node** list_new() {
+  auto s = new node*;
+  *s = nullptr;
+  return s;
 }
 
 void list_add(int val, node** head) {
-    auto last_node = *head;
+  auto last_node = *head;
 
-    if (*head != nullptr) {
-        for (auto pi = *head; pi != nullptr; pi = pi->next) {
-            last_node = pi;
-        }
+  if (*head != nullptr) {
+    for (auto pi = *head; pi != nullptr; pi = pi->next) {
+      last_node = pi;
     }
-    auto new_node = new node;
-    if (*head == nullptr) {
-        *head = new_node;
-    } else {
-        last_node->next = new_node;
-    }
-    new_node->next = nullptr;
-    new_node->value = val;
+  }
+  auto new_node = new node;
+  if (*head == nullptr) {
+    *head = new_node;
+  } else {
+    last_node->next = new_node;
+  }
+  new_node->next = nullptr;
+  new_node->value = val;
 }
 
 node** list_map(int (*f)(int val), node** head) {
-    auto r = list_empty();
+  auto r = list_new();
 
-    if (*head == nullptr) return r;
+  if (*head == nullptr) return r;
 
-    for (auto pi = *head; pi != nullptr; pi = pi->next) {
-        auto val = pi->value;
-        auto new_val = (*f)(val);
-        list_add(new_val, r);
-    }
+  for (auto pi = *head; pi != nullptr; pi = pi->next) {
+    auto val = pi->value;
+    auto new_val = (*f)(val);
+    list_add(new_val, r);
+  }
 
-    return r;
+  return r;
 }
 
 int double_val(int val) {
-    return val * 2;
+  return val * 2;
 }
 
 // this is done with syntactic sugar in ADPL
 node** create_list(std::vector<int> values) {
-    auto list = list_empty();
-    auto current_node = *list;
-    
-    for (size_t i = 0; i < values.size(); ++i) {
-        auto value = values[i];
-        auto new_addr = new node;
-        if (i == 0) {
-            *list = new_addr;
-        } else {
-            current_node->next = new_addr;
-        }
-        new_addr->next = nullptr;
-        new_addr->value = value;
-        current_node = new_addr;
+  auto list = list_new();
+  auto current_node = *list;
+  
+  for (size_t i = 0; i < values.size(); ++i) {
+    auto value = values[i];
+    auto new_addr = new node;
+    if (i == 0) {
+      *list = new_addr;
+    } else {
+      current_node->next = new_addr;
     }
-    
-    return list;
+    new_addr->next = nullptr;
+    new_addr->value = value;
+    current_node = new_addr;
+  }
+  
+  return list;
 }
 
 // this is a builtin function in ADPL
 void printList(node** head) {
-    std::cout << '[';
-    for (auto cur = *head; cur != nullptr; cur = cur->next) {
-        std::cout << cur->value;
-        if (cur->next != nullptr) std::cout << ',';
-    }
-    std::cout << ']' <<  std::endl;
+  std::cout << '[';
+  for (auto cur = *head; cur != nullptr; cur = cur->next) {
+    std::cout << cur->value;
+    if (cur->next != nullptr) std::cout << ',';
+  }
+  std::cout << ']' <<  std::endl;
 }
 
 int main() {
-    node** list = create_list({1,2,3});
-    printList(list);
+  node** list = create_list({1,2,3});
+  printList(list);
 
-    auto new_list = list_map(&double_val,list);
+  auto new_list = list_map(&double_val,list);
 
-    printList(new_list);
+  printList(new_list);
 
-    delete list;
-    delete new_list;
+  delete list;
+  delete new_list;
 
-    return 0;
+  return 0;
 }
